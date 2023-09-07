200b4b20643ca226cd564bd377f8c11a80b2318b
Tetiana Tsar <protetsar@gmail.com>
2023-09-07 13:21:36 +0300

    rewrite font export

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,4 @@
 import styles from './page.module.css'
-import PrimaryButton from './components/commonComponents/PrimaryButton/PrimaryButton'
 export default function Home() {
   return (
     <div className={styles.main}>
