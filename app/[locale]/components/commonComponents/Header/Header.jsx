1e000b606f2312e5c02760518511244fb7a27820
OlgaMykhailova <rosebud_z@yahoo.com>
2023-09-07 14:20:04 +0300

    added example of usage internalization in header component

--- app/[locale]/components/commonComponents/Header/Header.jsx
+++ app/[locale]/components/commonComponents/Header/Header.jsx
@@ -1,17 +1,22 @@
 "use client";
-import { useTranslations } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 import Link from "next/link";
 import styles from "./header.module.css";
 
 const Header = () => {
   const t = useTranslations("Header");
+  const locale = useLocale();
   return (
     <>
       <nav className={styles.nav}>
-        <Link href="/">Home</Link>
-        <Link href="/study">Study</Link>
-        <Link href="/team">Our team</Link>
-        <Link href="/contact">Contacts</Link>
+        <Link href={`/${locale}`}>{t("home")}</Link>
+        <Link href={`/${locale}/study`}>{t("study")}</Link>
+        <Link href={`/${locale}/team`} locale={locale}>
+          {t("team")}
+        </Link>
+        <Link href={`/${locale}/contact`} locale={locale}>
+          {t("contact")}
+        </Link>
       </nav>
     </>
   );
