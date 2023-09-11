6721336fa6396de7c2e320bd45b1dff8972e5c0b
VladAntonov95 <vladislav.95antonov@gmail.com>
2023-09-11 20:21:15 +0300

    added about us section

---
+++ app/[locale]/components/commonComponents/AboutUs/AboutUs.jsx
@@ -0,0 +1,37 @@
+"use client";
+import Image from 'next/image';
+import styles from './aboutUs.module.scss';
+import { useLocale } from 'next-intl';
+
+const AboutUs = () => {
+const locale = useLocale();
+
+  return <section className={styles.aboutUs}>
+        <div className={styles.image}><Image src='/about-us.svg' alt='aboutUsImg' width={720} height={1000} /></div>
+        <div className={styles.rightside}>
+            <div className={styles.text}>
+            <p className={styles.header}>Про нас</p>
+            <p className={styles.text}>Навесні 2022 року засновниця проєкту, викладачка
+            Олена Чекрижова, на прохання друга розпочала 
+            курс англійської у його батальйоні. Олена прожила
+            кілька місяців на військовій базі, щоденно
+            викладаючи англійську, після чого навчала офіцерів
+            та особовий склад у штабах і місцях дислокації,
+            працювала з військовими онлайн.</p>
+            <p className={styles.text}>Перші місяці Олена неофіційно називала свою
+            ініціативу “Спеціальна освітня спецоперація”,
+            пізніше проєкт отримав назву ENG for UARMY, а
+            згодом запустився Youtube-канал “Армійська
+            англійська”. За перший рік діяльності до створення
+            проєкту і найрізноманітніших завдань долучилися
+            більше сотні волонтерів (українців і іноземців).</p>
+            <p className={styles.text}>Ми віримо, що мова – це зброя. Чим більше у нас
+            зброї, тим ближча наша перемога. І місія нашого
+            проєкту – озброїти українські збройні сили
+            англійською.</p>
+            </div>
+        </div>
+    </section>
+
+};
+export default AboutUs;
--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,8 +1,10 @@
 import HelpUsGrows from "./components/Home/HelpUsGrow/HelpUsGrow";
+import AboutUs from "./components/commonComponents/AboutUs/AboutUs";
 
 const Home = () => {
   return <>
     <h1>HomePage</h1>
+    <AboutUs />
     <HelpUsGrows />
     </>
 }
