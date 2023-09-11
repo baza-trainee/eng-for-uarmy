88493ed064f26b1dfbf5d0092bd1ce0de807a714
VladAntonov95 <vladislav.95antonov@gmail.com>
2023-09-11 23:43:07 +0300

    added partner section

---
+++ app/[locale]/components/commonComponents/Partners/Partners.jsx
@@ -0,0 +1,66 @@
+"use client";
+import React, {useState} from 'react';
+import styles from './partners.module.scss';
+import Image from 'next/image'
+import { useLocale } from 'next-intl';
+import Link from 'next/link';
+
+const Partners = () => {
+    const locale = useLocale();
+
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const scrollLeft = () => {
+        const container = document.getElementById('imagesContainer');
+        if (container) {
+            const newPosition = scrollPosition - 320;
+            if (newPosition >= 0) { 
+                container.scrollTo({ left: newPosition, behavior: 'smooth' });
+                setScrollPosition(newPosition);
+            } else {
+                container.scrollTo({ left: 0, behavior: 'smooth' });
+                setScrollPosition(0);
+            }
+        }
+    };
+
+    const scrollRight = () => {
+        const container = document.getElementById('imagesContainer');
+        if (container) {
+            const maxScroll = container.scrollWidth - container.clientWidth;
+            const newPosition = scrollPosition + 320;
+            if (newPosition <= maxScroll) { 
+                container.scrollTo({ left: newPosition, behavior: 'smooth' });
+                setScrollPosition(newPosition);
+            } else {
+                container.scrollTo({ left: maxScroll, behavior: 'smooth' });
+                setScrollPosition(maxScroll);
+            }
+        }
+    };
+
+    return <section className={styles.partners}>
+        <div><p className={styles.header}>ПАРТНЕРИ</p></div>
+        <div id="imagesContainer" className={styles.scrollContainer}>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+            <div className={styles.icons}><Image src="./partners/partnersImage.svg" alt="SVG Image" width={179} height={179} /></div>
+        </div>
+        <div id="scrollButtonContainer" className={styles.scrollButtonContainer}>
+            <button id="scrollButtonLeft" className={styles.scrollButton} onClick={scrollLeft}></button>
+            <button id="scrollButtonRight" className={styles.scrollButton} onClick={scrollRight}></button>
+        </div>
+        <div>
+            <p className={styles.helpItem}>
+                <Link href={`/${locale}/contact`} className={styles.helpBtn}>Become a partner or sponsor</Link>
+            </p>   
+        </div>
+    </section>
+
+};
+
+export default Partners;
--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -2,12 +2,14 @@
 import Hero from "./components/commonComponents/Hero/Hero";
 import HelpUsGrows from "./components/Home/HelpUsGrow/HelpUsGrow";
 import AboutUs from "./components/commonComponents/AboutUs/AboutUs";
+import Partners from "./components/commonComponents/Partners/Partners";
 
 const Home = () => {
   return <>
     <Hero />
     <AboutUs />
     <HelpUsGrows />
+    <Partners />
 </>
 }
 export default Home;
