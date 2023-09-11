5246fd2314fa3ab3fd51730032749f8ba55a571a
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-09-11 17:41:45 +0200

    Fixed button

--- app/[locale]/components/commonComponents/Hero/Hero.jsx
+++ app/[locale]/components/commonComponents/Hero/Hero.jsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from 'next/image'
-import styles from "./Hero.styles.scss"; 
+import "./Hero.styles.scss"; 
 import { useLocale } from 'next-intl';
+import { Facebook, Youtube, Instagram, Telegram} from './SvgComponent';
+import Link from 'next/link';
  
 const Hero = () => {
 const locale = useLocale();
@@ -12,22 +14,22 @@ const locale = useLocale();
                 <h1 className='firstHead'>Eng for UArmy</h1>
                 <h2 className='secondHead'>Break the language barrier to achieve victory</h2>
                 <ul>
-  <li className='li-item'><Image src="./arrow-small.svg" alt="SVG Image" width={35} height={35}/><p className='li-txt'> <strong> ENG for UArmy </strong>  is the first <strong> Military English</strong> online course in Ukraine.</p></li>
-  <li className='li-item'><Image src="./arrow-small.svg" alt="SVG Image" width={35} height={35}/><p className='li-txt'>Спеціалізована мовна підготовка за стандартом <strong> НАТО (STANAG 6001).</strong></p></li>
-  <li className='li-item'><Image src="./arrow-small.svg" alt="SVG Image" width={35} height={35}/><p className='li-txt'>Перший в Україні відкритий курс військової англійської.</p></li>
-</ul>
-                <button className="under-txt-btn">Start studying</button>
-                <button className="under-txt-btn2">Support the project</button>
+                    <li className='li-item'><Image src="./HeroImage/arrow-small.svg" alt="SVG Image" width={35} height={35} /><p className='li-txt'> <strong> ENG for UArmy </strong>  is the first <strong> Military English</strong> online course in Ukraine.</p></li>
+                    <li className='li-item'><Image src="./HeroImage/arrow-small.svg" alt="SVG Image" width={35} height={35}/><p className='li-txt'>Спеціалізована мовна підготовка за стандартом <strong> НАТО (STANAG 6001).</strong></p></li>
+                    <li className='li-item'><Image src="./HeroImage/arrow-small.svg" alt="SVG Image" width={35} height={35}/><p className='li-txt'>Перший в Україні відкритий курс військової англійської.</p></li>
+                </ul>
+                <Link href={`/${locale}/study`}><button className="under-txt-btn">Start studying</button></Link>
+                <Link href={`/${locale}/helpUsGrow`}><button className="under-txt-btn2">Support the project</button></Link>
             </div>
 
             <div className='contact-us-col'>
-                <button className='contact-us'>contact us</button>
-                <button className='insagram social-btn'><Image src="./instagram.svg" alt="SVG Image" width={35} height={35}/></button>
-                <button className='facebook social-btn'><Image src="./facebook.svg" alt="SVG Image" width={13} height={25}/></button>
-                <button className='telegram social-btn'><Image src="./telegram.svg" alt="SVG Image" width={35} height={35}/></button>
+                <Youtube/>
+                <Instagram/>
+                <Facebook/>
+                <Telegram/>
                 </div>
         </div>
-        <Image className='soldierImg' src="/soldier-transp.png" alt="soldierImg" width={961} height={632}/>
+        <Image className='soldierImg' src="/HeroImage/soldier-transp.png" alt="soldierImg" width={961} height={632}/>
     </section>
   
 };
