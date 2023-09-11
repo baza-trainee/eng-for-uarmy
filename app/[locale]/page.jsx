bca2f19503e6774dca17c91eaacbe624a0be967a
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-09-11 18:35:16 +0200

    fix

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,15 +1,16 @@
 
-import styles from "./page.module.css";
+import HelpUsGrows from "./components/Home/HelpUsGrow/HelpUsGrow";
 import PrimaryButton from "./components/commonComponents/PrimaryButton/PrimaryButton";
 import Hero from "./components/commonComponents/Hero/Hero";
 
 const Home = () => {
   return (
-    <div className={styles.main}>
+    <>
       <h1>HomePage</h1>
       <Hero></Hero>
+      <HelpUsGrows />
       <PrimaryButton name="Start Studying" />
-    </div>
+    </>
   );
 
 }
