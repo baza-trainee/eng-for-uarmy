0e1354ee1bed4b8447cd5e091220c5181c1e9f8c
Tetiana Tsar <protetsar@gmail.com>
2023-09-14 01:49:49 +0300

    add two lang packege

--- app/[locale]/components/Home/Media/Media.jsx
+++ app/[locale]/components/Home/Media/Media.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from "next/link";
-import { useLocale } from "next-intl";
+import { useTranslations, useLocale } from "next-intl";
 import Image from "next/image";
 import MediaList from "./MediaList";
 import medias from "@/app/[locale]/libs/fakeMediaList";
@@ -8,14 +8,15 @@ import styles from "./Media.module.scss";
 import utilsStyles from '../../../styles/_utils.module.scss'
 
 const Media = () => {
+    const t = useTranslations("Media");
     const locale = useLocale();
-
+    
     return <section className={styles.section}>
         <div className={styles.mediaSection}>
-            <h2 className={`${utilsStyles.h2} ${styles.mediaTitle}`} >Медіа про ENG for UArmy</h2>
+            <h2 className={`${utilsStyles.h2} ${styles.mediaTitle}`}>{t("title")}</h2>
             <MediaList list={medias} />
             <button type="button" id='wrap' className={styles.arrowBtn}><Image src='/arrowWrap.svg' width={110} height={110} alt='media logo'/></button>
-            <Link Link href={`/${locale}/contact`} className={styles.mediaBtn}>Share our project with your audience</Link>
+            <Link Link href={`/${locale}/contact`} className={styles.mediaBtn}>{t("button")}</Link>
         </div>
     </section>
 }
