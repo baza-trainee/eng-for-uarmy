dc7394924292f0366254f7bb6f4b70c0cfa101e9
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-09-13 20:01:09 +0300

    add section testimonials
    slider don't works

--- app/[locale]/page.jsx
+++ app/[locale]/page.jsx
@@ -1,33 +1,20 @@
-<<<<<<< Updated upstream
 import Hero from "./components/Home/Hero/Hero";
 import HelpUsGrows from "./components/Home/HelpUsGrow/HelpUsGrow";
 import OurProjects from "./components/commonComponents/OurProjects/OurProjects";
 import AboutUs from "./components/Home/AboutUs/AboutUs";
 import Partners from "./components/Home/Partners/Partners";
 import RunningLine from "./components/Home/RunningLine/RunningLine";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 const Home = () => {
   return <>
     <Hero />
     <RunningLine/>
+    <Testimonials />
     <OurProjects />
     <AboutUs />
     <HelpUsGrows />
     <Partners />
 </>
-=======
-import styles from "./page.module.css";
-import PrimaryButton from "./components/commonComponents/PrimaryButton/PrimaryButton";
-import Testimonials from "./components/Testimonials/Testimonials";
 
-export default function Home() {
-  return (
-    <div className={styles.main}>
-      <h1>HomePage</h1>
-      <Testimonials />
-      <PrimaryButton name="Start Studying" />
-    </div>
-  );
->>>>>>> Stashed changes
-}
 export default Home;
