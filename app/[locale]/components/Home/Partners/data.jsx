244174edab778536052c4f19164ced720c48909a
VladAntonov95 <vladislav.95antonov@gmail.com>
2023-09-15 00:47:48 +0300

    addet swiper to partners section

---
+++ app/[locale]/components/Home/Partners/data.jsx
@@ -0,0 +1,92 @@
+import partner_1 from './imageOfPartners/partnersImage.svg';
+import partner_2 from './imageOfPartners/partnersImage.svg';
+import partner_3 from './imageOfPartners/partnersImage.svg';
+import partner_4 from './imageOfPartners/partnersImage.svg';
+import partner_5 from './imageOfPartners/partnersImage.svg';
+import partner_6 from './imageOfPartners/partnersImage.svg';
+import partner_7 from './imageOfPartners/partnersImage.svg';
+import partner_8 from './imageOfPartners/partnersImage.svg';
+import partner_9 from './imageOfPartners/partnersImage.svg';
+import partner_10 from './imageOfPartners/partnersImage.svg';
+import partner_11 from './imageOfPartners/partnersImage.svg';
+import partner_12 from './imageOfPartners/partnersImage.svg';
+import partner_13 from './imageOfPartners/partnersImage.svg';
+import partner_14 from './imageOfPartners/partnersImage.svg';
+
+
+
+    
+
+const data = [
+    {
+        id: 1,
+        src: partner_1,
+        alt: "Fhoto of the partner one",
+    },
+    {
+        id: 2,
+        src: partner_2,
+        alt: "Fhoto of the partner two",
+    }, 
+    {
+        id: 3,
+        src: partner_3,
+        alt: "Fhoto of the partner three",
+    }, 
+    {
+        id: 4,
+        src: partner_4,
+        alt: "Fhoto of the partner four",
+    }, 
+    {
+        id: 5,
+        src: partner_5,
+        alt: "Fhoto of the partner five",
+    }, 
+    {
+        id: 6,
+        src: partner_6,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 7,
+        src: partner_7,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 8,
+        src: partner_8,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 9,
+        src: partner_9,
+        alt: "Fhoto of the partner six",
+    }, {
+        id: 10,
+        src: partner_10,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 11,
+        src: partner_11,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 12,
+        src: partner_12,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 13,
+        src: partner_13,
+        alt: "Fhoto of the partner six",
+    }, 
+    {
+        id: 14,
+        src: partner_14,
+        alt: "Fhoto of the partner six",
+    }
+]
+
+export default data;
