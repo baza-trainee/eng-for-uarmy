3970e07543ade946710749c91a7feb1a694dc151
Tetiana Tsar <protetsar@gmail.com>
2023-09-22 00:13:47 +0300

    add mobile styles to modal

--- app/[locale]/components/Home/Modal/Modal.jsx
+++ app/[locale]/components/Home/Modal/Modal.jsx
@@ -14,7 +14,7 @@ const Modal = () => {
     });
     return <div className={styles.backdrop} onClick={() => onCloseClick()}>
         <div className={styles.modal} onClick={(e) => {e.stopPropagation()}}>
-            <button onClick={() => onCloseClick()}>
+            <button className={styles.btnCross} onClick={() => onCloseClick()}>
                 <svg className={styles.iconCross} width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fillRule="evenodd" clipRule="evenodd" d="M19.2932 20.0003L10 29.2935L10.7071 30.0006L20.0003 20.7074L29.2935 30.0006L30.0007 29.2935L20.7074 20.0003L30.0006 10.7071L29.2934 10L20.0003 19.2932L10.7072 10L10.0001 10.7071L19.2932 20.0003Z" fill="#231F20"/>
                 </svg>
@@ -54,10 +54,10 @@ const Modal = () => {
             </ul>
             <div className={styles.infoCard}>
                 <h4 className={styles.infoTitle}>{t("bankDetails")}</h4>
-                <p className={styles.infoItem}>Отримувач: БО «Благодійний фонд «ОСВІТУМ»</p>
-                <p className={styles.infoItem}>КОД ЄДРПОУ 44966920</p>
+                <p className={styles.infoItem}>Отримувач: <br className={styles.dived}/> БО «Благодійний фонд «ОСВІТУМ»</p>
+                <p className={styles.infoItem}>КОД ЄДРПОУ: <br className={styles.dived}/> 44966920</p>
                 <p className={styles.infoItem}>БАНК  УКРСИББАНК: п/р UA373510050000026007879183309</p>
-                <p className={styles.infoItem}>Призначення: благодійний внесок</p>
+                <p className={styles.infoItem}>Призначення: <br className={styles.dived}/> благодійний внесок</p>
             </div>
           </div>
       </div>
