1844febc82f901f059b2fdd7746bd3008bb714fa
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-10-02 15:05:02 +0300

    add condition for Thanks section

--- app/[locale]/components/ContactUs/ContactForm/ContactForm.jsx
+++ app/[locale]/components/ContactUs/ContactForm/ContactForm.jsx
@@ -9,6 +9,7 @@ import btnStyles from '../../commonComponents/MainLink/MainLink.module.scss';
 const ContactForm = () => {
   const [requestType, setRequestType] = useState("Type of request");
   const [isLoading, setIsLoading] = useState(false);
+  const [isSubmit, setIsSubmit] = useState(false)
 
   const handleInput = (e) => {
     const textarea = e.target;
@@ -52,6 +53,7 @@ const { values, errors, touched, handleBlur, handleChange, handleSubmit, setFiel
 
       setRequestType("Type of request");
       resetForm();
+      setIsSubmit(true);
     } catch (err) {
         console.log(err);
     } finally {
@@ -61,73 +63,78 @@ const { values, errors, touched, handleBlur, handleChange, handleSubmit, setFiel
 });
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
-      <div className={styles.form__wrapper}>
-        <div className={styles.form__blockLeft}>
-          <CustomSelect requestType={requestType} setRequestType={setRequestType}/>  
+    <>
+      {!isSubmit
+        ? <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.form__wrapper}>
+            <div className={styles.form__blockLeft}>
+              <CustomSelect requestType={requestType} setRequestType={setRequestType}/>  
 
-          <label className={styles.form__field}>
-            <input type="text"
-              name="name"
-              value={values.name}
-              placeholder=''
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={`${styles.form__input} ${styles.form__inputName}`} />
-            <span className={styles.form__label}>Your name</span>
-            {errors.name && touched.name && <p>{errors.name}</p>}
-          </label>
+              <label className={styles.form__field}>
+                <input type="text"
+                  name="name"
+                  value={values.name}
+                  placeholder=''
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`${styles.form__input} ${styles.form__inputName}`} />
+                <span className={styles.form__label}>Your name</span>
+                {errors.name && touched.name && <p>{errors.name}</p>}
+              </label>
 
-          <label className={styles.form__field}>
-            <input type="email"
-              name="email"
-              value={values.email}
-              placeholder=''
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={styles.form__input} /> 
-            <span className={styles.form__label}>Your email</span>
-            {errors.email && touched.email && <p>{errors.email}</p>}
-          </label>
-            {/* <FieldForm>Images
-                <InputForm
-                    type="file"
-                    name="images"
-                    multiple 
-                    onChange={e => setFieldValue('images', e.currentTarget.files)}
-                    />
-            </FieldForm> */}
-          
-        </div> 
+              <label className={styles.form__field}>
+                <input type="email"
+                  name="email"
+                  value={values.email}
+                  placeholder=''
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={styles.form__input} /> 
+                <span className={styles.form__label}>Your email</span>
+                {errors.email && touched.email && <p>{errors.email}</p>}
+              </label>
+                {/* <FieldForm>Images
+                    <InputForm
+                        type="file"
+                        name="images"
+                        multiple 
+                        onChange={e => setFieldValue('images', e.currentTarget.files)}
+                        />
+                </FieldForm> */}
+              
+            </div> 
+            
+            <div className={styles.form__blockRight}>
+              <label className={`${styles.form__field} ${styles.form__fieldTextarea}`}>
+                <textarea
+                  name="request"
+                  value={values.request}
+                  cols="30"
+                  rows="10"
+                  placeholder=''
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className={`${styles.form__input} ${styles.form__textarea}`}
+                  onInput={handleInput} />
+                <span className={`${styles.form__label} ${styles.form__labelTextarea}`}>
+                  Tell us more about your request</span>
+                {errors.request && touched.request && <p>{errors.request}</p>}
+              </label>
+            </div>
+          </div>  
+
+          <div className={styles.form__btnWrapper}>
+            <button type="submit"
+              disabled={isLoading}
+              className={`${btnStyles.mainLink} ${styles.form__btn}`}>
+              {isLoading && <span className={styles.form__spinner}></span>}
+              Send
+            </button>
+          </div>
+        </form >
         
-        <div className={styles.form__blockRight}>
-          <label className={`${styles.form__field} ${styles.form__fieldTextarea}`}>
-            <textarea
-              name="request"
-              value={values.request}
-              cols="30"
-              rows="10"
-              placeholder=''
-              onChange={handleChange}
-              onBlur={handleBlur}
-              className={`${styles.form__input} ${styles.form__textarea}`}
-              onInput={handleInput} />
-            <span className={`${styles.form__label} ${styles.form__labelTextarea}`}>
-              Tell us more about your request</span>
-            {errors.request && touched.request && <p>{errors.request}</p>}
-          </label>
-        </div>
-      </div>  
-
-      <div className={styles.form__btnWrapper}>
-        <button type="submit"
-          disabled={isLoading}
-          className={`${btnStyles.mainLink} ${styles.form__btn}`}>
-          {isLoading && <span className={styles.form__spinner}></span>}
-          Send
-        </button>
-      </div>
-    </form> 
+      : <div>Thanks</div>}
+    </>   
   );
 };
 
