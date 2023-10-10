f2000a607181e37528baadde4f156b9a80fcbbb2
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2023-10-10 21:43:58 +0200

    feature/btnUp
    changed visibility, fixed bugs

--- app/[locale]/components/commonComponents/ArowUp/ArowUp.jsx
+++ app/[locale]/components/commonComponents/ArowUp/ArowUp.jsx
@@ -1,19 +1,44 @@
 import { Link as ScrollLink } from 'react-scroll';
 import styles from "./ArowUp.module.scss";
+import { useEffect, useState } from 'react';
 
-const scrollToTop = () => {
+
+export const ArowUp = () => {
+  const [showArow, setShowArow] = useState(false);
+
+
+  const handleScroll = () => {
+    if (window.scrollY > 400) {
+      setShowArow(true);
+    } else {
+      setShowArow(false);
+    }
+  };
+
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
   };
 
-export const ArowUp = () => {
-    return <ScrollLink  to="top" onClick={scrollToTop} className={styles.ArowUpbtn} aria-label="Go up to the top of the page">
+    return <div>
+    {showArow && (
+    <ScrollLink  to="top" onClick={scrollToTop} className={styles.ArowUpbtn} aria-label="Go up to the top of the page">
                 <svg className={styles.svgArrowUp} viewBox="0 0 88 91" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle className={styles.Circle} cx="45.5" cy="41.5" r="37.5" fill="#F9F9F9"/>
                         <circle className={styles.ArowBorder} cx="45.5" cy="41.5" r="37" stroke="#231F20"/>
                         <path className={styles.Arow} fillRule="evenodd" clipRule="evenodd" d="M32.9715 40.4994L44.6388 28.4022L45.0003 28.0273L45.3601 28.4038L56.9721 40.554L56.2492 41.2449L45.4961 29.9934L45.4368 56.0288L44.4368 56.0265L44.4961 29.9907L33.6913 41.1936L32.9715 40.4994Z" fill="#231F20"/>
                     </svg>
       </ScrollLink>
+      )}
+      </div>
   }
