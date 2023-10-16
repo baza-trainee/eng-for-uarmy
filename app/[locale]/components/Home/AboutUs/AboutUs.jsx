261a66663eb53786a981bff09fec6940afbe9b55
VladAntonov95 <vladislav.95antonov@gmail.com>
2023-10-16 13:49:38 +0300

    improved picture quality

--- app/[locale]/components/Home/AboutUs/AboutUs.jsx
+++ app/[locale]/components/Home/AboutUs/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => {
             <div className={styles.rightside}>
                 <div className={styles.mobileContent}>
                 <h2 className={styles.title}>{t("title")}</h2>
-                <Image className={styles.mobileImage} src='/about-us-mobile.jpg' alt='mobile about us image' width={288} height={275} />
+                <Image className={styles.mobileImage} src='/about-us-mobile.svg' alt='mobile about us image' width={288} height={238} />
                 <p className={styles.paragraphOne}>{t("first-part")}</p>
                 <p className={styles.paragraphTwo}>{t("second-part")}</p>
                 <p className={styles.paragraphThree}>{t("third-part")}</p>
