489ffc4c6ae3a4f02e4cc4c557a34a54cfd67110
VladAntonov95 <vladislav.95antonov@gmail.com>
2023-11-24 15:32:58 +0200

    added conditions file and locale for conditions

---
+++ app/[locale]/components/Conditions/conditions.jsx
@@ -0,0 +1,12 @@
+import styles from "./conditions.module.scss";
+
+const Conditions = () => {
+  return (
+    <section className={styles.conditionsSection}>
+      <div>
+        <p>Умови користування</p>
+      </div>
+    </section>
+  );
+};
+export default Conditions;
