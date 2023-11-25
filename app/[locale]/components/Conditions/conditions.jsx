51c10d34c4a54baf535729f3c001763905eb87de
VladAntonov95 <vladislav.95antonov@gmail.com>
2023-11-25 16:17:57 +0200

    feature/add-conditional-page

--- app/[locale]/components/Conditions/conditions.jsx
+++ app/[locale]/components/Conditions/conditions.jsx
@@ -1,10 +1,117 @@
 import styles from "./conditions.module.scss";
+import { useTranslations } from "next-intl";
 
 const Conditions = () => {
+  const t = useTranslations("termsOfUse");
+
   return (
     <section className={styles.conditionsSection}>
       <div>
-        <p>Умови користування</p>
+        <div className={styles.headerArea}>
+          <h1 className={styles.header}>{t("header")}</h1>
+        </div>
+        <div className={styles.dateArea}>
+          <p className={styles.date}>{t("startDate")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("header-text1")}</p>
+          <p className={styles.text}>{t("header-text2")}</p>
+          <p className={styles.text}>{t("header-text3")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("service")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("service-text1")}</p>
+          <p className={styles.text}>{t("service-text2")}</p>
+          <p className={styles.text}>{t("service-text3")}</p>
+          <p className={styles.text}>{t("service-text4")}</p>
+          <p className={styles.text}>{t("service-text5")}</p>
+          <p className={styles.text}>{t("service-text6")}</p>
+          <p className={styles.text}>{t("service-text7")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("donations")}</p>
+        </div>
+        <div clasName={styles.textArea}>
+          <p className={styles.donationFirstParagraph}>
+            {t("donations-text1")}
+          </p>
+          <p className={styles.donationSecondParagraph}>
+            {t("donations-text2")}
+          </p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("intellectualProperty")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("intellectualProperty-text1")}</p>
+          <p className={styles.text}>{t("intellectualProperty-text2")}</p>
+          <p className={styles.text}>{t("intellectualProperty-text3")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("links")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("links-text1")}</p>
+          <p className={styles.text}>{t("links-text2")}</p>
+          <p className={styles.text}>{t("links-text3")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("indemnification")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("indemnification-text1")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("limitationOfLiability")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("limitationOfLiability-text1")}</p>
+          <ul>
+            <li>
+              <p className={styles.listText}>
+                {t("limitationOfLiability-text2")}
+              </p>
+            </li>
+            <li>
+              <p className={styles.listText}>
+                {t("limitationOfLiability-text3")}
+              </p>
+            </li>
+            <li>
+              <p className={styles.listText}>
+                {t("limitationOfLiability-text4")}
+              </p>
+            </li>
+          </ul>
+          <p className={styles.text}>{t("limitationOfLiability-text6")}</p>
+          <p className={styles.text}>{t("limitationOfLiability-text7")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("governingLaw")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.text}>{t("governingLaw-text1")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("changes")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.changesFirstParagraph}>{t("changes-text1")}</p>
+          <p className={styles.changesSecondParagraph}>{t("changes-text2")}</p>
+        </div>
+        <div className={styles.titleArea}>
+          <p className={styles.title}>{t("contactUs")}</p>
+        </div>
+        <div className={styles.textArea}>
+          <p className={styles.contactUsFirstParagraph}>
+            {t("contactUs-text1")}
+          </p>
+          <p className={styles.contactUsSecondParagraph}>
+            {t("contactUs-text2")}
+          </p>
+        </div>
       </div>
     </section>
   );
