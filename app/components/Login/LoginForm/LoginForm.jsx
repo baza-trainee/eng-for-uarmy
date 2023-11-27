44979738fdc219803b1e045e994ef6f332430056
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-11-27 23:29:31 +0200

    add button styles

--- app/components/Login/LoginForm/LoginForm.jsx
+++ app/components/Login/LoginForm/LoginForm.jsx
@@ -1,25 +1,30 @@
 import styles from "./LoginForm.module.scss";
+import btnStyles from "../../commonComponents/MainLink/MainLink.module.scss";
 
 const LoginForm = () => {
     return (
-        <form className={styles.form}>
+        <form className={styles.form} autoComplete="off">
             <h2 className={styles.form__title}>ВХІД</h2>
 
             <label className={styles.form__label}>
                 Логін
                 <input type="email"
-                    className={styles.form__input}/>
+                    className={styles.form__input}
+                    placeholder="Введіть логін"/>
             </label>
 
             <label className={styles.form__label}>
                 Пароль
                 <input type="password"
-                    className={styles.form__input}/>
+                    className={styles.form__input}
+                    placeholder="Введіть пароль"/>
             </label>
 
-            <a href="">Забули пароль?</a>
+            <a href="" className={styles.form__link}>Забули пароль?</a>
 
-            <button type="submit">Увійти</button>
+            <div>
+                <button type="submit" className={btnStyles.mainLink}>Увійти</button>
+            </div>
             
         </form>
     )
