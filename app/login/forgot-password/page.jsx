316c6f549fc11f74db9c4b772c72d8c28d566998
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-11-28 14:42:27 +0200

    add forgot password page

---
+++ app/login/forgot-password/page.jsx
@@ -0,0 +1,7 @@
+import ForgotPassword from "../../components/Login/ForgotPassword/ForgotPassword";
+
+const ForgotPasswordPage = () => {
+  return <ForgotPassword />
+}
+
+export default ForgotPasswordPage
