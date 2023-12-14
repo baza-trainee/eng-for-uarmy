f25c39ccee6b9626f8ebadc1b8e99661bb971738
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-12-14 15:24:34 +0200

    change imports for images for our team page

--- app/components/OurTeam/MemberList/MemberList.jsx
+++ app/components/OurTeam/MemberList/MemberList.jsx
@@ -1,14 +1,16 @@
 "use client";
 import { useTranslations } from "next-intl";
-import styles from './MemberList.module.scss';
 import Image from 'next/image';
+import memberURLs from "./images";
+import styles from './MemberList.module.scss';
+;
 
 const MemberList = () => {
     const t = useTranslations("OurTeam");
 
     const members = Array.from({ length: 24 }, (_, idx) => ({
         id: idx + 1,
-        photo: `/team/member-${String(idx + 1).padStart(2, '0')}.webp`,
+        photo: memberURLs[idx], 
         name: t(`name-${idx + 1}`),
         role: t(`role-${idx + 1}`),
     }));
@@ -17,14 +19,12 @@ const MemberList = () => {
         <ul className={styles.members__list}>
             {members?.map(({ id, photo, name, role }) =>
                 <li key={id} className={styles.members__item}>
-                    <Image
-                        src={photo}
+                    <Image src={photo}
                         alt={name}
                         width={130}
                         height={150}
                         className={styles.members__img}
-                        loading='lazy'
-                    />
+                        loading='lazy' />
                     
                     <div className={styles.members__info}>
                         <h3 className={styles.members__name}>{name}</h3>
---
+++ app/components/OurTeam/MemberList/images.jsx
@@ -0,0 +1,32 @@
+import member_1 from '../../../../assets/images/team/member-01.webp';
+import member_2 from '../../../../assets/images/team/member-02.webp';
+import member_3 from '../../../../assets/images/team/member-03.webp';
+import member_4 from '../../../../assets/images/team/member-04.webp';
+import member_5 from '../../../../assets/images/team/member-05.webp';
+import member_6 from '../../../../assets/images/team/member-06.webp';
+import member_7 from '../../../../assets/images/team/member-07.webp';
+import member_8 from '../../../../assets/images/team/member-08.webp';
+import member_9 from '../../../../assets/images/team/member-09.webp';
+import member_10 from '../../../../assets/images/team/member-10.webp';
+import member_11 from '../../../../assets/images/team/member-11.webp';
+import member_12 from '../../../../assets/images/team/member-12.webp';
+import member_13 from '../../../../assets/images/team/member-13.webp';
+import member_14 from '../../../../assets/images/team/member-14.webp';
+import member_15 from '../../../../assets/images/team/member-15.webp';
+import member_16 from '../../../../assets/images/team/member-16.webp';
+import member_17 from '../../../../assets/images/team/member-17.webp';
+import member_18 from '../../../../assets/images/team/member-18.webp';
+import member_19 from '../../../../assets/images/team/member-19.webp';
+import member_20 from '../../../../assets/images/team/member-20.webp';
+import member_21 from '../../../../assets/images/team/member-21.webp';
+import member_22 from '../../../../assets/images/team/member-22.webp';
+import member_23 from '../../../../assets/images/team/member-23.webp';
+import member_24 from '../../../../assets/images/team/member-24.webp';
+
+const memberURLs = [
+    member_1, member_2, member_3, member_4, member_5, member_6, member_7, member_8, member_9, member_10,
+    member_11, member_12, member_13, member_14, member_15, member_16, member_17, member_18, member_19, member_20,
+    member_21, member_22, member_23, member_24,
+];
+
+export default memberURLs;
