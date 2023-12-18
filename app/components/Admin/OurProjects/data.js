9b3b79352d63e067294dc6c84b5a0e6736b67589
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-12-18 18:37:28 +0200

    adds data

---
+++ app/components/Admin/OurProjects/data.js
@@ -0,0 +1,32 @@
+import project_1 from "./images/project-1-original.webp";
+import project_2 from "./images/project-2-original.webp";
+import project_3 from "./images/project-3-original.webp";
+
+const dataProjects = [
+	{
+		id: 1,
+		title_en: "Video course “Army English”",
+		title_ua: "Відео-курс “Армійська Англійська”",
+		text_en: "Regular video lessons based on STANAG 6001 on the YouTube channel allow you to learn Military English anywhere and anytime, from basic grammar and vocabulary to more specialized military topics.",
+		text_ua: "Регулярні авторські відео-уроки на YouTube-каналі дозволяють вивчати Military English за стандартом STANAG 6001  будь-де і будь-коли, починаючи з базової граматики та лексики до більш спеціалізованих тем.",
+		src: project_1,
+	},
+	{
+		id: 2,
+		title_en: "Speaking practice with speakers of English",
+		title_ua: "Розмовна практика з носіями мови",
+		text_en: "We bring together Ukrainian military personnel and English-speaking volunteers for conversational online meetings and practice communicating in English.",
+		text_ua: "Ми об’єднуємо українських військовослужбовців, ветеранів та перевірених волонтерів-носіїв англійської мови для розмовних онлайн-зустрічей та практики спілкування англійською.",
+		src: project_2,
+	},
+	{
+		id: 3,
+		title_en: "English courses for units of the Armed Forces",
+		title_ua: "Курси англійської для підрозділів ЗСУ",
+		text_en: "During the span of a year, Olena Chekryzhova, teacher and the founder of the project, held regular classes in groups of units of the Armed Forces, making it possible to analyze the peculiarities of military language training and to plan a video course.",
+		text_ua: "Протягом року засновниця проєкту Олена Чекрижова проводила регулярні заняття в групах підрозділів ЗСУ, що дало можливість вивчити запит та особливості навчання військових та спланувати відео-курс.",
+		src: project_3,
+	}
+];
+
+export default dataProjects;
