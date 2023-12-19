f84cc330f2964f928620f82fe3e3511d43ef9c5c
Kate-Lesnyak <catharinalesnyak@gmail.com>
2023-12-19 19:16:49 +0200

    Update page.jsx

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,11 +1,9 @@
 import Review from "../components/Admin/Review/Review";
-import OurProjects from "../components/Admin/OurProjects/OurProjects";
 
 const AdminPage = () => {
   return (
     <>
       <Review />
-      <OurProjects />
     </>
   );
 };
