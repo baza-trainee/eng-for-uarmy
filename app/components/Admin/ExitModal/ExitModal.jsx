1976184322fb97c6a7ce421cb6e738bbe1eb3fc8
Antifishka <101981622+Antifishka@users.noreply.github.com>
2023-12-21 15:47:31 +0200

    add hower for icons

--- app/components/Admin/ExitModal/ExitModal.jsx
+++ app/components/Admin/ExitModal/ExitModal.jsx
@@ -17,13 +17,9 @@ const ExitModal = ({ onClose }) => {
         onClick={onClose}
         aria-label="Close exit modal"
         className={styles.btnCross}>
-        <svg width="24" height="24"
-          viewBox="0 0 40 40"
-          fill="none">
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M19.2932 20.0003L10 29.2935L10.7071 30.0006L20.0003 20.7074L29.2935 30.0006L30.0007 29.2935L20.7074 20.0003L30.0006 10.7071L29.2934 10L20.0003 19.2932L10.7072 10L10.0001 10.7071L19.2932 20.0003Z"
+        <svg width="24" height="24" viewBox="0 0 40 40" fill="none">
+          <path fillRule="evenodd"
+            clipRule="evenodd" d="M19.2932 20.0003L10 29.2935L10.7071 30.0006L20.0003 20.7074L29.2935 30.0006L30.0007 29.2935L20.7074 20.0003L30.0006 10.7071L29.2934 10L20.0003 19.2932L10.7072 10L10.0001 10.7071L19.2932 20.0003Z"
             fill="#231F20"/>
         </svg>
       </button>
