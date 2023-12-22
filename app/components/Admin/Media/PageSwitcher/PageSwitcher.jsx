aa42058e7f60505c8ff851809b56e80c8d0e3621
Tetiana Tsar <protetsar@gmail.com>
2023-12-22 13:55:16 +0200

    create page Switcher

--- app/components/Admin/Media/PageSwitcher/PageSwitcher.jsx
+++ app/components/Admin/Media/PageSwitcher/PageSwitcher.jsx
@@ -1,18 +1,20 @@
-const PageSwitcher = ({currentPage, setCurrentPage, allMedia}) => {
-   
+import styles from "./PageSwitcher.module.scss";
+
+const PageSwitcher = ({ currentPage, setCurrentPage, allMedia }) => {
+    const allPages = Math.ceil(allMedia / 5);
     const onChangePage = (type) => {
         type === 'next' ? setCurrentPage(currentPage + 1) : setCurrentPage(currentPage - 1);
     }
-    return <div>
-        <button type="button" onClick={()=> onChangePage()}>back</button>
-        <div>
-            <span>{currentPage}</span>
+
+    return <div className={styles.switcherBox}>
+        <button className={styles.arrowBtn} disabled={currentPage === 1} type="button" onClick={()=> onChangePage()}>&lt;</button>
+        <div className={styles.pageBox}>
+            <span className={styles.pageNumber}>{currentPage}</span>
             з
-            <span>{Math.ceil(allMedia / 5)}</span>
+            <span className={styles.pageNumber}>{allPages}</span>
         </div>
-        <button type="button" onClick={()=> onChangePage("next")}>далі</button>
-    
-</div>
+        <button className={styles.arrowBtn} disabled={currentPage === allPages} type="button" onClick={()=> onChangePage("next")}>&gt;</button>   
+    </div>
 }
 
 export default PageSwitcher;
